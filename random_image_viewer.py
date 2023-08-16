2eda4fcee7fc994ff2aea22e55558bfe7b50ae16
Dongjin Lee <dongjin@MacBook-Pro.lan>
2023-08-15 23:49:19 -0700

    image_to_skip added

--- random_image_viewer.py
+++ random_image_viewer.py
@@ -1,6 +1,7 @@
 import os
 import random
 from PIL import Image
+import keyboard
 
 # Path to your folder containing image files
 image_folder = "./imgs"
@@ -9,12 +10,27 @@ image_folder = "./imgs"
 image_files = [file for file in os.listdir(image_folder) if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
 
 # Shuffle the image files
-random.shuffle(image_files)
+image_to_skip = set()
+no_skip_imgs = len(image_files) - len(image_to_skip)
 
-for image_file in image_files:
-    image_path = os.path.join(image_folder, image_file)
-    image = Image.open(image_path)
-    image.show()
-    input("Press Enter to close the image and view the next image...")
+while no_skip_imgs > 0:
+    random.shuffle(image_files)
+    print("Number of images:", no_skip_imgs)
 
-print("No more images to display.")
+    img_num = 1
+    for image_file in image_files:
+        image_path = os.path.join(image_folder, image_file)
+        
+        if image_path in image_to_skip:
+            continue
+        
+        image = Image.open(image_path)
+        image.show()
+        
+        user_input = input(f"Image {img_num}/{len(image_files)}. Press 1 to skip, any key to continue: ")
+        img_num += 1
+
+        if user_input == "1":
+            image_to_skip.add(image_path)
+
+    no_skip_imgs = len(image_files) - len(image_to_skip)
